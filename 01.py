3d909deb2f872a90687dd0374fd03686d0c38eb7
Tommy H. Yeargin, Jr <tommyyearginjr@users.noreply.github.com>
2017-05-18 06:50:00 -0400

    Update 01.py

--- 01.py
+++ 01.py
@@ -2,7 +2,7 @@ def MyChart():
 	import datetime
 	date = datetime.datetime.today()
 	info = input('Info: ')
-	print('{} {}'.format(date,info))
+	print('{:%Y%m%d%H%M} {}'.format(date,info))
 	f = open('MyHealthChart.txt','a')
 	f.write('{:%Y%m%d%H%M} {}\n'.format(date,info))
 	f.close()
